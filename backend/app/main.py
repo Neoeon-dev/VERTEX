b35146b0d44af9ad6365187d294814f017708dc3
Neoeon <neoeon.dev@gmail.com>
2026-09-06 00:31:00 +0530

    V1

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,6 +45,7 @@ app.add_middleware(
         "http://localhost:80",
         "http://localhost",
         "https://vertex-mca6cp0i3-neoeon-devs-projects.vercel.app",
+        "*",
 
     ],
     allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
